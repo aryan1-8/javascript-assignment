33c7c210341d408bfa59db218309e908b6af5a46
Aryan patel <patelaryan1801@gmail.com>
2026-08-18 22:32:31 +0530

    Add exam practical React app

---
+++ exam-ractical/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Pro1 from './Component/pro1'
+
+function App() {
+  return (
+    <>
+      <Pro1/>
+    </>
+  )
+}
+
+export default App
---
+++ exam-ractical/src/Component/Pro1.jsx
@@ -0,0 +1,8 @@
+function Pro1(){
+    return(
+        <>
+        <h1>Hello</h1>
+        </>
+    );
+}
+export default Pro1;
